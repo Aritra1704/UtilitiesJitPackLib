01c22b971941bfa618988a98ac9a780de9d5ace1
Aritra <aritra1704@gmail.com>
2016-11-17 17:19:31 +0530

    Added 1st letter caps method in StringUtils.

--- utilitieslib/src/main/java/com/arpaul/utilitieslib/StringUtils.java
+++ utilitieslib/src/main/java/com/arpaul/utilitieslib/StringUtils.java
@@ -101,4 +101,23 @@ public class StringUtils {
         }
         return listString;
     }
+
+    public static String wordFirstCap(String str) {
+        String[] words = str.trim().split(" ");
+        StringBuilder ret = new StringBuilder();
+        for(int i = 0; i < words.length; i++)
+        {
+            if(words[i].trim().length() > 0)
+            {
+                LogUtils.errorLog("words[i].trim",""+words[i].trim().charAt(0));
+                ret.append(Character.toUpperCase(words[i].trim().charAt(0)));
+                ret.append(words[i].trim().substring(1));
+                if(i < words.length - 1) {
+                    ret.append(' ');
+                }
+            }
+        }
+
+        return ret.toString();
+    }
 }
