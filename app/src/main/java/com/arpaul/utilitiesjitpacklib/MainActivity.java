c8b0bd0190b9c43a83202ded8134b3636100d670
aritrapal <aritra.pal@zippr.in>
2017-09-14 17:16:21 +0530

    Added Network speed check.

--- app/src/main/java/com/arpaul/utilitiesjitpacklib/MainActivity.java
+++ app/src/main/java/com/arpaul/utilitiesjitpacklib/MainActivity.java
@@ -3,10 +3,14 @@ package com.arpaul.utilitiesjitpacklib;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.arpaul.utilitieslib.CalendarUtils;
 import com.arpaul.utilitieslib.LogUtils;
+import com.arpaul.utilitieslib.NetworkUtility;
 import com.arpaul.utilitieslib.PermissionUtils;
 import com.arpaul.utilitieslib.StringUtils;
 import com.arpaul.utilitieslib.UnCaughtException;
@@ -25,30 +29,38 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        if(new PermissionUtils().checkPermission(this,new String[]{
-                android.Manifest.permission.ACCESS_FINE_LOCATION,
-                android.Manifest.permission.ACCESS_COARSE_LOCATION}) != 0){
+//        if(new PermissionUtils().checkPermission(this,new String[]{
+//                android.Manifest.permission.ACCESS_FINE_LOCATION,
+//                android.Manifest.permission.ACCESS_COARSE_LOCATION}) != 0){
+//
+//            new PermissionUtils().verifyPermission(this,new String[]{
+//                    android.Manifest.permission.ACCESS_FINE_LOCATION,
+//                    android.Manifest.permission.ACCESS_COARSE_LOCATION});
+//        } else {
+//
+//        }
+//
+//        StringUtils.getDouble("0.001");
+//        StringUtils.getFloat("0.1");
+//        StringUtils.getInt("1");
+//        StringUtils.getLong("1");
+//        StringUtils.removeLastComma("1,2,3,");
+//        StringUtils.getMeterToMile(10);
+//
+//        String current_day = CalendarUtils.getDateinPattern(CalendarUtils.DATE_FORMAT_WITH_COMMA);
+//        LogUtils.infoLog(TAG , current_day);
+//
+//        String dateinReqFormat = CalendarUtils.getDateinPattern("2016-09-05", CalendarUtils.DATE_FORMAT1, CalendarUtils.DATE_FORMAT_WITH_COMMA);
+//        LogUtils.infoLog(TAG , dateinReqFormat);
+//
+//        LogUtils.debugLog(TAG, "" + ValidationUtils.validatePhoneNumber("903030303407"));
 
-            new PermissionUtils().verifyPermission(this,new String[]{
-                    android.Manifest.permission.ACCESS_FINE_LOCATION,
-                    android.Manifest.permission.ACCESS_COARSE_LOCATION});
-        } else {
+        findViewById(R.id.btnInternetSpeed).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((TextView)findViewById(R.id.tvInternet)).setText(NetworkUtility.getInternetSpeed(MainActivity.this));
+            }
+        });
 
-        }
-
-        StringUtils.getDouble("0.001");
-        StringUtils.getFloat("0.1");
-        StringUtils.getInt("1");
-        StringUtils.getLong("1");
-        StringUtils.removeLastComma("1,2,3,");
-        StringUtils.getMeterToMile(10);
-
-        String current_day = CalendarUtils.getDateinPattern(CalendarUtils.DATE_FORMAT_WITH_COMMA);
-        LogUtils.infoLog(TAG , current_day);
-
-        String dateinReqFormat = CalendarUtils.getDateinPattern("2016-09-05", CalendarUtils.DATE_FORMAT1, CalendarUtils.DATE_FORMAT_WITH_COMMA);
-        LogUtils.infoLog(TAG , dateinReqFormat);
-
-        LogUtils.debugLog(TAG, "" + ValidationUtils.validatePhoneNumber("903030303407"));
     }
 }
--- utilitieslib/src/main/java/com/arpaul/utilitieslib/NetworkUtility.java
+++ utilitieslib/src/main/java/com/arpaul/utilitieslib/NetworkUtility.java
@@ -3,6 +3,9 @@ package com.arpaul.utilitieslib;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.telephony.TelephonyManager;
 
 /**
  * Created by Aritra on 10-08-2016.
@@ -87,5 +90,44 @@ public class NetworkUtility {
             connection_type = CONNECTION_TYPE.TYPE_NOT_CONNECT;
         }
         return connection_type;
+    }
+
+    public static String getInternetSpeed(Context context) {
+        StringBuilder internetSpeed = new StringBuilder();
+        String NOCONNECTION = "NO-CONNECTION ", POOR = "POOR ", AVERAGE = "AVERAGE ", GOOD = "AVERAGE ";
+        String WIFI = "WIFI ", MOBILE = "MOBILE ";
+        int speed = 0;
+
+        switch (getConnectionType(context)) {
+            case TYPE_WIFI:
+                WifiManager wifiMgr = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+                WifiInfo wifiInfo = wifiMgr.getConnectionInfo();
+                speed = wifiInfo.getLinkSpeed();
+                internetSpeed.append(WIFI);
+                break;
+
+            case TYPE_MOBILE:
+                TelephonyManager teleMgr = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+                speed = teleMgr.getNetworkType();
+                internetSpeed.append(MOBILE);
+                break;
+
+            case TYPE_NOT_CONNECT:
+                speed = 0;
+                break;
+        }
+
+        internetSpeed.append("network ");
+
+        if(speed <= 0)
+            internetSpeed.append(NOCONNECTION);
+        else if(speed > 0 && speed < 20)
+            internetSpeed.append(POOR);
+        else if(speed > 20 && speed < 80)
+            internetSpeed.append(AVERAGE);
+        else if(speed >= 80)
+            internetSpeed.append(GOOD);
+
+        return internetSpeed.toString();
     }
 }
